2dd36b291a8777403951ccc6876f90472e6be48a
AlexV18 <71417996+AlexV18@users.noreply.github.com>
2021-12-18 00:35:42 -0500

    Added text fields and submit button.

---
+++ script.js
@@ -0,0 +1,10 @@
+firstName = document.getElementById("first");
+lastName = document.getElementById("last");
+email = document.getElementById("email");
+password = document.getElementById("password");
+repassword = document.getElementById("repassword");
+
+function createAccount()
+{
+    
+}
