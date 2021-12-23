e15201418f1f5d27524558ad14c5adf6a3e74d6c
AlexV18 <71417996+AlexV18@users.noreply.github.com>
2021-12-23 17:41:44 -0500

    Added login system functionality and top bar

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-firstName = document.getElementById("first");
-lastName = document.getElementById("last");
-email = document.getElementById("email");
-password = document.getElementById("password");
-repassword = document.getElementById("repassword");
+
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const repassword = document.getElementById("repassword");
+
+const error = document.getElementById("error");
+
+let name1 = "";
+let name2 = "";
+let emailVar = "";
+let passwordVar = "";
 
 function createAccount()
 {
-    
+    name1 = firstName.value;
+    name2 = lastName.value;
+    emailVar = email.value;
+    passwordVar = password.value; 
+
+    repasswordVar = repassword.value;
+
+    if(password !== repasswordVar)
+    {
+        error.style.opacity = 1;
+    }
 }
+
+
