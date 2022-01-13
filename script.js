96e0b27ff2e5e5840c0eeef091f82ef1ef67600c
AlexV18 <71417996+AlexV18@users.noreply.github.com>
2022-01-12 21:01:50 -0500

    Implemented Multiple Additions
    - Added login page
    - Added startup page
    - Added login functionality (yet it doesn't seem to work just yet).

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ const repassword = document.getElementById("repassword");
 
 const error = document.getElementById("error");
 
+const emailOne = document.getElementById("email-one");
+const passOne = document.getElementById("password-one");
+
 let name1 = "";
 let name2 = "";
 let emailVar = "";
 let passwordVar = "";
+
+const emailArray = [];
+const passArray = [];
 
 function createAccount(event)
 {
@@ -24,16 +30,46 @@ function createAccount(event)
 
     repasswordVar = repassword.value;
 
-    console.log(password.value);
-    console.log(repassword.value);
     if(passwordVar !== repasswordVar)
     {
         error.style.opacity = 1;
     }
     else 
     {
+        emailArray.push(emailVar);
+        passArray.push(passwordVar);
         window.location.href = "./anatomy.html";
     }
 }
 
+//Log out functionality for the "anatomy" page:
+function logOut()
+{
+    window.location.href = "./startupPage.html"
+}
+
+
+function authenticate(event)
+{
+    event.preventDefault();
+
+    emailVar = emailOne.value;
+    passwordVar = passOne.value; 
+
+    for (let i=0; i<emailArray.length; i++) {
+        if(emailVar === emailArray[i] && passwordVar === passArray[i]) {
+            window.location.href = "./anatomy.html"
+        }
+    }
+}
+
+function logIn()
+{
+    window.location.href = "./loginPage.html"
+}
+
+function signUp()
+{
+    window.location.href = "./index.html"
+}
 
